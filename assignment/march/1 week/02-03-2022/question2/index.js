0b7d1bb5e217eb8c88ccae0139a68537616eab1f
Chetanprojec <96860008+Chetanprojec@users.noreply.github.com>
2022-03-06 17:03:06 +0530

    Add files via upload

---
+++ assignment/march/1 week/02-03-2022/question2/index.js
@@ -0,0 +1,223 @@
+
+var counter = 0 ;
+
+function like(){
+    counter += 1;
+    render()
+   
+}
+
+function dislike(){
+    counter -= 1;
+    render()
+    
+}
+
+function render(){
+    var disp = document.getElementById("firstimg");
+    disp.innerText = counter;
+}
+
+// second function
+var counter1 = 0
+function like1(){
+    counter1 += 1;
+    render1()
+   
+}
+
+function dislike1(){
+    counter1 -= 1;
+    render1()
+    
+}
+
+function render1(){
+    var disp = document.getElementById("secondimg");
+    disp.innerText = counter1;
+}
+//third function
+var counter2 = 0
+function like2(){
+    counter2 += 1;
+    render2()
+   
+}
+
+function dislike2(){
+    counter2 -= 1;
+    render2()
+    
+}
+
+function render2(){
+    var disp = document.getElementById("thirdimg");
+    disp.innerText = counter2;
+}
+
+//fouth function
+var counter3 = 0
+function like3(){
+    counter3 += 1;
+    render3()
+   
+}
+
+function dislike3(){
+    counter3 -= 1;
+    render3()
+    
+}
+
+function render3(){
+    var disp = document.getElementById("fourthimg");
+    disp.innerText = counter3;
+}
+//fifth function
+var counter4 = 0
+function like4(){
+    counter4 += 1;
+    render4()
+   
+}
+
+function dislike4(){
+    counter4 -= 1;
+    render4()
+    
+}
+
+function render4(){
+    var disp = document.getElementById("fifthimg");
+    disp.innerText = counter4;
+}
+
+//fifth function
+var counter5 = 0
+function like5(){
+    counter5 += 1;
+    render5()
+   
+}
+
+function dislike5(){
+    counter5 -= 1;
+    render5()
+    
+}
+
+function render5(){
+    var disp = document.getElementById("sixthimg");
+    disp.innerText = counter5;
+}
+//sixth function
+var counter6 = 0
+function like6(){
+    counter6 += 1;
+    render6()
+   
+}
+
+function dislike6(){
+    counter6 -= 1;
+    render6()
+    
+}
+
+function render6(){
+    var disp = document.getElementById("seventhimg");
+    disp.innerText = counter6;
+}
+//seventh function
+var counter7 = 0
+function like7(){
+    counter7 += 1;
+    render7()
+   
+}
+
+function dislike7(){
+    counter7 -= 1;
+    render7()
+    
+}
+
+function render7(){
+    var disp = document.getElementById("eighthimg");
+    disp.innerText = counter7;
+}
+//eighth function
+var counter8 = 0
+function like8(){
+    counter8 += 1;
+    render8()
+   
+}
+
+function dislike8(){
+    counter8 -= 1;
+    render8()
+    
+}
+
+function render8(){
+    var disp = document.getElementById("ninethimg");
+    disp.innerText = counter8;
+}
+
+//nineth function
+var counter9 = 0
+function like9(){
+    counter9 += 1;
+    render9()
+   
+}
+
+function dislike9(){
+    counter9 -= 1;
+    render9()
+    
+}
+
+function render9(){
+    var disp = document.getElementById("tenthimg");
+    disp.innerText = counter9;
+}
+
+//thenth function
+var counter10 = 0
+function like10(){
+    counter10 += 1;
+    render10()
+   
+}
+
+function dislike10(){
+    counter10 -= 1;
+    render10()
+    
+}
+
+function render10(){
+    var disp = document.getElementById("eleventhimg");
+    disp.innerText = counter10;
+}
+
+//eleventh function
+var counter11 = 0
+function like11(){
+    counter11 += 1;
+    render11()
+   
+}
+
+function dislike11(){
+    counter11 -= 1;
+    render11()
+    
+}
+
+function render11(){
+    var disp = document.getElementById("twelvethimg");
+    disp.innerText = counter11;
+}
